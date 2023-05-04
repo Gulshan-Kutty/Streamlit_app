1f8a93cab02869b80e7da07d18ee79cc5658150d
Gulshan-Kutty <122353040+Gulshan-Kutty@users.noreply.github.com>
2023-05-04 11:25:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.header('Hello World')
+st.title('Hello')
+st.header('Welcome to Data :red[Science Internship] 2023')
+st.subheader('Good Morning:sunglasses:')
+
+st.write('##########################################')
+
+# var = '''print('Hello World')'''
+# st.code(var ,language='python')
+
+# st.write('##########################################')
+
+# df = pd.DataFrame(
+#    np.random.randn(50, 20),
+# columns=('col %d' % i for i in range(20)))
+
+# st.dataframe(df)
+
+# st.write('##########################################')
+
+btn_click = st.button('Click kar be ith!')
+
+if btn_click == True:
+    st.subheader('Match Lavachi ka aaj?:sunglasses:')
+    st.balloons()
+
+st.write('##########################################')
+st.snow()
