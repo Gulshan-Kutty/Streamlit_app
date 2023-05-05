07335a28e44322285ab5839525cab3dc53696e62
Gulshan-Kutty <122353040+Gulshan-Kutty@users.noreply.github.com>
2023-05-05 14:25:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,29 +3,28 @@ import pandas as pd
 import numpy as np
 
 st.title('Hello')
-st.header('Welcome to :red[the Library]')
-#st.header('Welcome to :red[Data Science Internship] 2023')
+st.header('Welcome to :red[Data Science Internship] 2023')
 st.subheader('Good Morning:sunglasses:')
 
-# st.write('##########################################')
+st.write('##########################################')
 
-# var = '''print('Hello World')'''
-# st.code(var ,language='python')
+var = '''print('Hello World')'''
+st.code(var ,language='python')
 
-# st.write('##########################################')
+st.write('##########################################')
 
-# df = pd.DataFrame(
-#    np.random.randn(50, 20),
-# columns=('col %d' % i for i in range(20)))
+df = pd.DataFrame(
+   np.random.randn(50, 20),
+columns=('col %d' % i for i in range(20)))
 
-# st.dataframe(df)
+st.dataframe(df)
 
-# st.write('##########################################')
+st.write('##########################################')
 
 btn_click = st.button('Click Me!')
 
 if btn_click == True:
-    st.subheader('You are looking gorgeous today, lets go for a date this month:gift_heart:')
+    st.subheader('Welcome Gulshan:sunglasses:')
     st.balloons()
 
 st.write('##########################################')
